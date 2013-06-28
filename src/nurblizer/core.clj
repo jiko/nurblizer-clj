e78b48b2a472d681d4b5902df57fe0756ce60d10
James Koppen <jk@jameskoppen.com>
2013-06-28 00:23:50 -0500

    Fixed indention and refactored a bit, but still seems like there's a more idiomatic or straightforward way of doing this.

--- src/nurblizer/core.clj
+++ src/nurblizer/core.clj
@@ -1,46 +1,49 @@
 (ns nurblizer.core
-      (:gen-class)
-      (:use compojure.core)
-      (:require
-        [clojure.string :as str]
-        [clostache.parser :as clostache]
-        [ring.adapter.jetty :as ring]
-        [compojure.route :as route]
-        [compojure.handler :as handler]))
+    (:gen-class)
+    (:use compojure.core)
+    (:require
+      [clojure.string :as str]
+      [clostache.parser :as clostache]
+      [ring.adapter.jetty :as ring]
+      [compojure.route :as route]
+      [compojure.handler :as handler]))
 
-    ;; main nurble stuff
-    (def nouns                                                     ; map nouns into a set
-      (set (map (comp str/trim str/upper-case)                     ; upper-case and trim lines
-                (-> (slurp (clojure.java.io/resource "nouns.txt")) ; load in nouns.txt
-                    (str/split #"\n")))))                          ; split by line
+  ;; main nurble stuff
+  (def nouns                                                     ; map nouns into a set
+    (set (map (comp str/trim str/upper-case)                     ; upper-case and trim lines
+              (-> (slurp (clojure.java.io/resource "nouns.txt")) ; load in nouns.txt
+                  (str/split #"\n")))))                          ; split by line
 
 
 
-    (def nurble-replacement-text "<span class=\"nurble\">nurble</span>")
+  (def nurble-replacement-text "<span class=\"nurble\">nurble</span>")
 
-    (defn nurble-word [word]
-      (get nouns (str/upper-case word) nurble-replacement-text)) ; return word if word in set else nurble
+  (defn nurble-word [word]
+    (get nouns (str/upper-case word) nurble-replacement-text)) ; return word if word in set else nurble
 
-    (defn nurble [text]
-      (str/replace (str/replace (str/upper-case text) #"[^A-Z ]" "") 
-        #"\n|\w+" #(case %              ; using anon func literal, switch on arg
-                    "\n" "<br>"         ; when arg is newline replace with br
-                    (nurble-word %))))  ; otherwise nurble the argument (a word)
+  (defn normalize [text]
+    (str/replace (str/upper-case text) #"[^A-Z ]" ""))
 
-    ;; webserver stuff
-    (defn read-template [template-file]
-      (slurp (clojure.java.io/resource (str "templates/" template-file ".mustache"))))
+  (defn nurble [text]
+    (str/replace (normalize text) 
+      #"\n|\w+" #(case %             ; using anon func literal, switch on arg
+                  "\n" "<br>"        ; when arg is newline replace with br
+                  (nurble-word %)))) ; otherwise nurble the argument (a word)
 
-    (defn render
-      ([template-file params]
-       (clostache/render (read-template template-file) params
-                         {:_header (read-template "_header")
-                          :_footer (read-template "_footer") })))
+  ;; webserver stuff
+  (defn read-template [template-file]
+    (slurp (clojure.java.io/resource (str "templates/" template-file ".mustache"))))
 
-    (defroutes main-routes
-      (GET "/"        []     (render "index" {}))
-      (POST "/nurble" [text] (render "nurble" {:text (nurble text)}))
-      (route/resources "/static"))
+  (defn render
+    ([template-file params]
+     (clostache/render (read-template template-file) params
+                       {:_header (read-template "_header")
+                        :_footer (read-template "_footer") })))
 
-    (defn -main []
-      (ring/run-jetty (handler/site main-routes) {:port 9000}))
+  (defroutes main-routes
+    (GET "/"        []     (render "index" {}))
+    (POST "/nurble" [text] (render "nurble" {:text (nurble text)}))
+    (route/resources "/static"))
+
+  (defn -main []
+    (ring/run-jetty (handler/site main-routes) {:port 9000}))
