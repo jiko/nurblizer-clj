293645fdd7d35c50334135db7420b0f0eaf1b880
James Koppen <jk@jameskoppen.com>
2013-06-27 21:43:58 -0500

    Updated dependencies to newer versions.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject nurblizer "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring/ring-core "1.1.8"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [de.ubercode.clostache/clostache "1.3.1"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.5"]]
   :main nurblizer.core
   )
